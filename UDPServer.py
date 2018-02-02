9f79ae008f1d0936f44bcf4d371a26ef3717966e
zhangxuetonghub <36028125+zhangxuetonghub@users.noreply.github.com>
2018-02-02 15:10:51 +0800

    Update UDPServer.py

--- UDPServer.py
+++ UDPServer.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python
+from socket import *
+from time import ctime
+
+HOST = '127.0.0.1'
+PORT = 12123
+BUFSIZE = 1024
+ADDR = (HOST,PORT)
+
+udpSerSock = socket(AF_NET, SOCK_DGRAM)
+udpSerSock = 
