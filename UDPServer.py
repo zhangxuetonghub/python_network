80c07dd859905b2cc7ec16d2f72ed5252e27e8e4
zhangxuetonghub <36028125+zhangxuetonghub@users.noreply.github.com>
2018-02-02 16:17:06 +0800

    Update UDPServer.py

--- UDPServer.py
+++ UDPServer.py
@@ -7,5 +7,12 @@ PORT = 12123
 BUFSIZE = 1024
 ADDR = (HOST,PORT)
 
-udpSerSock = socket(AF_NET, SOCK_DGRAM)
-udpSerSock = 
+udpSerSock = socket(AF_INET, SOCK_DGRAM)
+udpSerSock.bind(ADDR)
+
+while True:
+    print("waiting for message...")
+    data, addr = udpSerSock.recvfrom(BUFSIZE)
+    udpSerSock.sendto(bytes('[%s] %s' % (ctime(),data.decode('utf-8')),'utf-8'),addr)
+    print("...received form and returned to :",addr)
+udpSerSock.close()
